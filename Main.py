1c5e60a68934357d71838cab7a71e76645b30181
Nikhil Sebastian <nikhiljs@hotmail.com>
2015-07-07 00:40:23 -0400

    Test code: read xml document. Print the font style elements and size element within the font style elements.

--- Main.py
+++ Main.py
@@ -1,1 +1,11 @@
 __author__ = 'Nikhil'
+
+import xml.etree.ElementTree as ET
+tree = ET.parse("AuchanFranceCommon.xml")
+root = tree.getroot()
+for styleElement in root.iter("Style"):
+    for fontElement in styleElement.iter("Font"):
+        if fontElement.text == "Arial":
+            print(styleElement.attrib, end="")
+            for sizeElement in styleElement.iter("Size"):
+                print(" Size =  "+sizeElement.text)
