de2c181dbbf447ae189ea379ea8e71a612e8bab3
nikhiljs <humpudumpu@gmail.com>
2015-07-07 17:25:46 -0400

    Add code to find the "Font" tag with "Arial Narrow" font face

--- Main.py
+++ Main.py
@@ -1,11 +1,29 @@
 __author__ = 'Nikhil'
 
 import xml.etree.ElementTree as ET
-tree = ET.parse("AuchanFranceCommon.xml")
-root = tree.getroot()
-for styleElement in root.iter("Style"):
-    for fontElement in styleElement.iter("Font"):
-        if fontElement.text == "Arial":
-            print(styleElement.attrib, end="")
-            for sizeElement in styleElement.iter("Size"):
-                print(" Size =  "+sizeElement.text)
+import shutil
+import os
+import contextlib
+import mmap
+
+multimediaFilePath = "C:\\Work\CustomSet\\Multimedia\\GroupCommon\\XML\\AuchanFranceCommon.xml"
+multimediaFilePath_Edited = "C:\\Work\CustomSet\\Multimedia\\GroupCommon\\XML\\AuchanFranceCommon_edited.xml"
+mmDoc = ET.parse(multimediaFilePath)
+root = mmDoc.getroot()
+if os.path.exists(multimediaFilePath_Edited):
+    os.remove(multimediaFilePath_Edited)
+shutil.copyfile(multimediaFilePath, multimediaFilePath_Edited)
+
+for styleNode in root.iter("Style"):
+    arialNarrow = False
+    styleNodeName = styleNode.attrib["Name"]
+    for subNodes in styleNode.getchildren():
+        if subNodes.tag == "Font" and subNodes.text == "Arial Narrow":
+            arialNarrow = True
+        if subNodes.tag == "Size" and arialNarrow:
+            print(" Name: ", styleNodeName, end=" ::  ")
+            print(" Current Size: ", subNodes.text, end=" <> ")
+            subNodes.text = int(subNodes.text) - 2
+            print(" New Size: ", subNodes.text)
+
+mmDoc.write(multimediaFilePath_Edited,None,None, None, None, True)
