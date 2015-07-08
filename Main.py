42f622ec3842e5464b00960c6cfa51e3dfcdaf2e
nikhiljs <humpudumpu@gmail.com>
2015-07-08 10:31:43 -0400

    Refactor: Move the code to a function.

--- Main.py
+++ Main.py
@@ -6,6 +6,22 @@ import os
 import contextlib
 import mmap
 
+# Function to find the style node with font = Arial Narrow
+def FindUpdateRequiredStyleNodes (root):
+    affectedFonts = []
+    for styleNodes in root.findall(".//MultiLang/Style[Font='Arial Narrow']"):
+        print(" Affected font node : "+ styleNodes.attrib["Name"], end=" <> ")
+        affectedFonts.append(styleNodes.attrib["Name"])
+        for fontNodes in styleNodes.findall(".//Font"):
+            print("Old font : " + fontNodes.text, end=" <> ")
+            fontNodes.text = "Arial"
+            print("New font : " + fontNodes.text, end=" <> ")
+        for sizeNodes in styleNodes.findall(".//Size"):
+            print("Old font size: ", sizeNodes.text, end=" <> ")
+            sizeNodes.text = int(sizeNodes.text) - 2
+            print("New font size: ", sizeNodes.text)
+    return affectedFonts
+
 multimediaFilePath = "AuchanFranceCommon.xml"
 multimediaFilePath_Edited = "AuchanFranceCommon_edited.xml"
 mmDoc = ET.parse(multimediaFilePath)
@@ -13,19 +29,5 @@ root = mmDoc.getroot()
 if os.path.exists(multimediaFilePath_Edited):
     os.remove(multimediaFilePath_Edited)
 
-affectedFonts = []
+affectedFonts = FindUpdateRequiredStyleNodes(root)
 
-for styleNodes in root.findall(".//MultiLang/Style[Font='Arial Narrow']"):
-    print(" Affected font node : "+ styleNodes.attrib["Name"])
-    affectedFonts.append(styleNodes.attrib["Name"])
-    for fontNodes in styleNodes.findall(".//Font"):
-        print("Old font : " + fontNodes.text, end=" <> ")
-        fontNodes.text = "Arial"
-        print("New font : " + fontNodes.text, end=" <> ")
-    for sizeNodes in styleNodes.findall(".//Size"):
-        print("Old font size: ", sizeNodes.text, end=" <> ")
-        sizeNodes.text = int(sizeNodes.text) - 2
-        print("New font size: ", sizeNodes.text)
-
-for affectedFont in affectedFonts:
-    print(affectedFont, end=" <> ")
